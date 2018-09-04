400c543289291c886f229c6e8c9a183a5bfdd5eb
Evgenii Kornaushenkov <evgenii_kornaushenkov@epam.com>
2018-09-04 21:33:08 +0300

    Add annotations for validation dto

--- src/main/java/com/andreitop/newco/dto/TripDto.java
+++ src/main/java/com/andreitop/newco/dto/TripDto.java
@@ -1,14 +1,26 @@
 package com.andreitop.newco.dto;
 
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 public class TripDto implements TripEntity, Serializable {
 
     private static final long serialVersionUID = 5914366185889783660L;
 
+    @NotNull
     private Long id;
+    @NotBlank
+    @Size(min = 3, message = "Length of the origin's airport name must be more than 3 symbols")
     private String origin;
+    @NotBlank
+    @Size(min = 3, message = "Length of the destination's airport name must be more than 3 symbols")
     private String destination;
+    @NotNull
+    @Range(min = 0, max = 500000)
     private Integer price;
 
     public Long getId() {
