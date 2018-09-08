75b5b47d4ea95e3d84939cfc5bda254c193b79ed
Evgenii Kornaushenkov <evgenii_kornaushenkov@epam.com>
2018-09-09 00:39:46 +0300

    Task 5 complete

--- src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
+++ src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
@@ -1,9 +1,13 @@
 package com.andreitop.newco.aspect;
 
 
+import com.andreitop.newco.dto.TripDto;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,7 +18,6 @@ public class LoggingAspect {
 
     private static final Logger logger = LogManager.getLogger(LoggingAspect.class);
 
-
     @Before("com.andreitop.newco.aspect.PointcutContainer.repositoryFind()")
     public void beforeRepoFind(JoinPoint joinPoint) {
         String className = joinPoint.getSignature().getDeclaringTypeName();
@@ -22,4 +25,22 @@ public class LoggingAspect {
         logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
     }
 
+    @Around("com.andreitop.newco.aspect.PointcutContainer.serviceMethods()")
+    public Object loggingServiceMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Service method " + className + "." + methodName + " is about to be called");
+        Object proceed = joinPoint.proceed();
+        logger.info(" ---> Service method " + className + "." + methodName + " has called");
+        return proceed;
+    }
+
+    @After("com.andreitop.newco.aspect.PointcutContainer.controllerMethodsWithTripDtoArg()")
+    public void loggingControllerMethodsWithTripDtoArg(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        TripDto arg = (TripDto) joinPoint.getArgs()[0];
+        logger.info(" ---> Controller method " + className + "." + methodName +
+                " with TripDto argument (id = " + arg.getId() + ") has called");
+    }
 }
--- src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
+++ src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
@@ -7,4 +7,20 @@ public class PointcutContainer {
     @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.find*())")
     public void repositoryFind() {
     }
+
+    @Pointcut("@within(org.springframework.stereotype.Service)")
+    public void serviceMethods() {
+    }
+
+    @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
+    public void controllerMethods() {
+    }
+
+    @Pointcut("args(com.andreitop.newco.dto.TripDto)")
+    public void methodsWithTripDtoArg() {
+    }
+
+    @Pointcut("controllerMethods() && methodsWithTripDtoArg()")
+    public void controllerMethodsWithTripDtoArg() {
+    }
 }
