7d4008e054986417603b1a9af20ef659a4ac67c6
Evgenii Kornaushenkov <evgenii_kornaushenkov@epam.com>
2018-09-09 19:41:10 +0300

    Task 6: add DataConfig class and TripJpaRepository interface

---
+++ src/main/java/com/andreitop/newco/repository/TripJpaRepository.java
@@ -0,0 +1,7 @@
+package com.andreitop.newco.repository;
+
+import com.andreitop.newco.dto.TripDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripJpaRepository extends JpaRepository<TripDto, Long> {
+}
